610291b5540fa7f62f084cfc77eb7f23c2630023
Emerson <emecasti65@gmail.com>
2025-05-07 15:13:45 -0500

    Reforma

--- middleware.js
+++ middleware.js
@@ -4,31 +4,41 @@ export function middleware(request) {
   // Leer la cookie 'visits' de la solicitud
   let visits = parseInt(request.cookies.get("visits")?.value || "0", 10);
 
+  console.log("Visitas actuales:", visits);
+
   // Aumentar el contador de visitas
   visits += 1;
 
-  // Crear la respuesta
-  const response = NextResponse.next();
-
   // Si el número de visitas es 3 o más, redirigir a YouTube
   if (visits >= 3) {
-    return NextResponse.redirect(
+    const redirectResponse = NextResponse.redirect(
       "https://micuenta.infonavit.org.mx/?gad_source=1"
     );
+    // Asegúrate que la cookie se actualiza ANTES de redirigir
+    redirectResponse.cookies.set("visits", visits.toString(), {
+      maxAge: 60 * 60 * 24 * 365, // 1 año
+      path: "/",
+      sameSite: "lax",
+      secure: true, // PON false si estás en localhost. Pon true si tienes dominio con https
+    });
+    return redirectResponse;
   }
 
-  // Si el número de visitas es menor que 3, continuar y actualizar la cookie
+  // Crear la respuesta normal
+  const response = NextResponse.next();
+
+  // Actualizar la cookie con la nueva visita
   response.cookies.set("visits", visits.toString(), {
-    maxAge: 60 * 60 * 24 * 365, // La cookie durará 1 año
-    path: "/", // La cookie funciona en toda la página
-    sameSite: "lax", // Lax es menos estricto y funciona bien en móviles
-    secure: true,
+    maxAge: 60 * 60 * 24 * 365, // 1 año
+    path: "/",
+    sameSite: "lax",
+    secure: true, // PON false si estás en localhost. Pon true si tienes dominio con https
   });
 
   return response;
 }
 
-// Configurar el middleware para que se ejecute solo en la ruta principal ('/')
+// Aplicar a todas las rutas
 export const config = {
   matcher: "/",
 };
