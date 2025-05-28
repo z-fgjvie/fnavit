52986e644551a4729efc6d9e000cfe6e65d6fe76
Emerson <emecasti65@gmail.com>
2025-05-28 12:39:06 -0500

    Actz

--- middleware.js
+++ middleware.js
@@ -3,23 +3,22 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 export function middleware(request) {
-  console.log("h");
-  // const visitas =
-  //   parseInt(request.cookies.get("visitas")?.value || "0", 10) + 1;
+  const visitas =
+    parseInt(request.cookies.get("visitas")?.value || "0", 10) + 1;
 
-  // const response =
-  //   visitas >= 3
-  //     ? NextResponse.redirect("https://micuenta.infonavit.org.mx/?gad_source=1")
-  //     : NextResponse.next();
+  const response =
+    visitas >= 3
+      ? NextResponse.redirect("https://micuenta.infonavit.org.mx/?gad_source=1")
+      : NextResponse.next();
 
-  // response.cookies.set("visitas", visitas.toString(), {
-  //   maxAge: 60 * 60 * 24 * 365,
-  //   path: "/",
-  //   sameSite: "lax",
-  //   httpOnly: true,
-  // });
+  response.cookies.set("visitas", visitas.toString(), {
+    maxAge: 60 * 60 * 24 * 365,
+    path: "/",
+    sameSite: "lax",
+    httpOnly: true,
+  });
 
-  // return response;
+  return response;
 }
 
 export const config = {
